94148f88099a06a983e2170307cbf8f4b6ad4d80
Markus Suorsa <markus.suorsa@gmail.com>
2017-09-07 11:41:10 -0700

    added micropython build

---
+++ spyrit/micropython/stmhal/spirit1/spirit1_SPI.c
@@ -0,0 +1,56 @@
+
+#include "MCU_Interface.h"
+#include "spi.h"
+#include "pin.h"
+
+extern skin_position;
+
+void SpiritSpiDeInit(void)
+{
+
+}
+
+void SpiritSpiInit(void)
+{
+
+}
+
+StatusBytes SpiritSpiWriteRegisters(uint8_t cRegAddress, uint8_t cNbBytes, uint8_t* pBuffer)
+{
+
+}
+
+StatusBytes SpiritSpiReadRegisters(uint8_t cRegAddress, uint8_t cNbBytes, uint8_t* pcBuffer)
+{
+
+}
+
+StatusBytes SpiritSpiCommandStrobes(uint8_t cCommandCode)
+{
+
+}
+
+StatusBytes SpiritSpiWriteFifo(uint8_t cNbBytes, uint8_t* pcBuffer)
+{
+
+}
+
+StatusBytes SpiritSpiReadFifo(uint8_t cNbBytes, uint8_t* pcBuffer)
+{
+
+}
+
+void SpiritEnterShutdown(void)
+{
+
+}
+
+void SpiritExitShutdown(void)
+{
+
+}
+
+void SpiritCheckShutdown(void)
+{
+
+}
